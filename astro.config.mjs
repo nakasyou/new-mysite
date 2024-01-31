306ff0735d72764e8870776943a23bf28d7fddfa
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2024-01-31 20:24:14 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  base: '/new-mysite'
 });
